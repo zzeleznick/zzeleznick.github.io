610cc95a36f625875f738d41209dee5f9dacb8ef
zzeleznick <zzeleznick@gmail.com>
2014-07-27 14:30:52 +0200

    Sought to upload images via client

--- angular-parse/app.js
+++ angular-parse/app.js
@@ -1,4 +1,4 @@
-//app key, js key
+//app id, js key
 
 Parse.initialize("GN9N9d0CCreAHC4kOX4xe690IHwNlaLsT3qARkFW", "el0yCIuUavEf3LL2edwR5pvKACltjK2WfEYLVPD8");
 
@@ -40,4 +40,43 @@ angular.module('AuthApp', [])
     Parse.User.logOut();
     $scope.currentUser = null;
   };
-}]);
+
+/*  $scope.upload = function(img) {
+
+    var file;
+    // Set an event listener on the Choose File field.
+
+    $('#fileselect').bind("change", function(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      // Our file var now holds the selected file
+      file = files[0];
+    });
+
+    // This function is called when the user clicks on Upload to Parse. It will create the REST API request to upload this image to Parse.
+   // $('#uploadbutton').click(function() {
+      var serverUrl = 'https://api.parse.com/1/files/' + file.name;
+
+      $.ajax({
+        type: "POST",
+        beforeSend: function(request) {
+          request.setRequestHeader("X-Parse-Application-Id", 'GN9N9d0CCreAHC4kOX4xe690IHwNlaLsT3qARkFW');
+          request.setRequestHeader("X-Parse-REST-API-Key", 'ckhVTy3BtgGiPm94W9dOfNhKL4wqneUyMyU6TTQ8');
+          request.setRequestHeader("Content-Type", file.type);
+        },
+        url: serverUrl,
+        data: file,
+        processData: false,
+        contentType: false,
+        success: function(data) {
+          alert("File available at: " + data.url);
+        },
+        error: function(data) {
+          var obj = jQuery.parseJSON(data);
+          alert(obj.error);
+        }
+      });  //end ajax
+
+    } //end upload action;
+    */
+
+}]);  //end app
