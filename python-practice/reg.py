cd9e9463b3691cef26a099a44472ef7778ae17c8
zzeleznick <zzeleznick@gmail.com>
2015-03-14 14:48:58 -0700

    used operators package to create a sorted array with the sorted function, and will mark as complete for now

--- python-practice/reg.py
+++ python-practice/reg.py
@@ -9,16 +9,21 @@ VERBOSE will allow the user to find the most commmonly used words up to the limi
 '''
 import argparse
 import re
+import operator
 def uniqueWordsCount(text):
     '''
     Returns the number of unique words
+    and the dictionary with all words
     non-latin words are excluded, and
     case is non-exclusive
     '''
     pattern = re.compile(r"""
         \s*                 # Skip leading whitespace
         [a-zA-Z]+           # any letter
-        [\s\-\n]+           # space hyphen or newline
+        [\s\-\n]+           # space hyphen or newline or colon
+    """, re.VERBOSE)
+    rx = re.compile(r"""
+        [:,\.]*             # hyphen or newline, comma, colon
     """, re.VERBOSE)
     print 'running uniqueWordsCount'
     i = 0
@@ -27,27 +32,56 @@ def uniqueWordsCount(text):
     with open(text, 'r') as f:
         for line in f:
             i+=1
-            matches = pattern.findall(line.strip(' \t\n')) #strip any spaces, tabs, newlines in pattern
+            trimline = re.sub(rx, '', line)
+            matches = pattern.findall(trimline)
             wordCount += len(matches)
-            print matches
-            print line
+            #print matches
+            #print line
             for match in matches:
+                match = match.strip(' \n') #remove spaces and newlines from matches in dict
                 if wordDict.__contains__(match):
                     wordDict.__setitem__(match, wordDict.get(match) + 1)
                 else:
                     wordDict.__setitem__(match, 1)
-            if i == 20 :
-                break
+            #if i == 10 :
+            #    break
     print 'total word count = ' + str(wordCount)
     print 'unique word count = ' +str(wordDict.__len__())
+    #print wordDict
     print 'end of uniqueWordsCount'
+    return wordDict
+
+def commonWords(mydict, limit):
+    '''
+    Returns the most
+    frequent words in a dictionary
+    and the count up to a limit
+    as the number of words to return
+    '''
+    count = 0
+    print 'limit passed as ' + str(limit)
+    #print 'length as ' + str(mydict.__len__())
+    #print limit > mydict.__len__()
+    if limit > mydict.__len__():
+        limit = mydict.__len__()
+        print 'limit exceeded. default to total words at: ' + str(limit)
+    sorted_x = sorted(mydict.items(), key=operator.itemgetter(1), reverse=True)
+    for item in sorted_x:
+        #tmp = max(mydict.iteritems(), key=operator.itemgetter(1)) # {'key', value} for max val
+        print item
+        count+=1
+        if count == limit:
+            break
+    print 'end of output'
+
 
 if  __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("file", help="display the number and word of unique words in a textfile")
+    parser.add_argument("lim", help="display the number of common words to display", type=int, nargs='?', default=1)
     parser.add_argument("-v", "--verbosity", action="count", default=0, help="increase output verbosity")
     args = parser.parse_args()
-    print args
+    #print args
     ''' if args.verbosity >= 2:
         #print("the square of {} equals {}".format(args.square, answer))
     elif args.verbosity >= 1:
@@ -55,4 +89,6 @@ if  __name__ == '__main__':
     else:
         #print(answer)
     '''
-    uniqueWordsCount(args.file)
+    x = uniqueWordsCount(args.file)
+    #print args.lim
+    commonWords(x, args.lim)
